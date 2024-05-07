1182d174d16093a7bbc1f4deaab102e0bd9a0764
nhan phan <nhanphan1159@gmail.com>
2024-05-07 15:59:54 +0700

    lastest new

---
+++ src/pages/Home/IMG/index.js
@@ -0,0 +1,22 @@
+import Trip from './Trip.png';
+import Jingle from './Jingle.png';
+import woman from './woman.png';
+
+export const IMG = [
+  {
+    img: woman,
+    title: 'Happy international women’s day!',
+    date: '15/03/2023',
+  },
+
+  {
+    img: Jingle,
+    title: 'Happy international women’s day!',
+    date: '15/03/2023',
+  },
+  {
+    img: Trip,
+    title: 'Happy international women’s day!',
+    date: '15/03/2023',
+  },
+];
